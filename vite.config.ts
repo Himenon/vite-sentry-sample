edc48a544d9c1311c406b9f370f9bf8fbe917e60
Himenon <k.himeno314@gmail.com>
2022-06-01 20:44:24 +0900

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import type { ViteSentryPluginOptions } from "vite-plugin-sentry";
+import viteSentry from "vite-plugin-sentry";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const sentryConfig: ViteSentryPluginOptions = {
+  url: "https://my.ondemand.sentry.com",
+  authToken: "<SECRET_TOKEN_HERE>",
+  org: "my_org",
+  project: "my_project",
+  release: "1.0",
+  deploy: {
+    env: "production",
+  },
+  setCommits: {
+    auto: true,
+  },
+  sourceMaps: {
+    include: ["./dist/assets"],
+    ignore: ["node_modules"],
+    urlPrefix: "~/assets",
+  },
+};
+
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), viteSentry(sentryConfig)],
+});
