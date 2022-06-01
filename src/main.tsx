eb848403258e4c2fea5557d76ddb8e3b71628932
K.Himeno <k.himeno314@gmail.com>
2022-06-01 22:51:27 +0900

    feat: add sentry (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+Sentry.init({
+  dsn: "https://6c504fbf92444eefa49976dae9ca8231@o1267824.ingest.sentry.io/6454582",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import type { ViteSentryPluginOptions } from "vite-plugin-sentry";
+import viteSentry from "vite-plugin-sentry";
+import react from "@vitejs/plugin-react";
+import * as pkg from "./package.json";
 
-// https://vitejs.dev/config/
+const env = process.env;
+
+const sentryConfig: ViteSentryPluginOptions = {
+  url: "https://sentry.io",
+  authToken: env.SENTRY_AUTH_TOKEN,
+  org: env.SENTRY_ORG,
+  project: env.SENTRY_PROJECT,
+  release: pkg.version,
+  deploy: {
+    env: env.SENTRY_ENVIRONMENT,
+  },
+  setCommits: {
+    auto: true,
+  },
+  sourceMaps: {
+    include: ["./dist/assets"],
+    ignore: ["node_modules"],
+    urlPrefix: "~/assets",
+  },
+};
+
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    env.DEPLOY_SENTRY !== "false" ? viteSentry(sentryConfig) : undefined,
+  ],
+});
