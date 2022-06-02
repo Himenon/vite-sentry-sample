b5a0121aa7343374258eed436de5abb2666d728a
Himenon <k.himeno314@gmail.com>
2022-06-02 10:42:37 +0900

    chore: add random error

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,16 @@ function App() {
     throw new Error("エラーだよー");
   }
 
+  const handleOnClick2 = () => {
+    throw new Error(`Please Fix Me: ${Math.random()}`);
+  }
+
   return (
     <div>
       <h1>Sentry Sample {version}</h1>
       <div>
         <button onClick={handleOnClick}>Throw Error</button>
+        <button onClick={handleOnClick2}>Random Error</button>
       </div>
     </div>
   )
