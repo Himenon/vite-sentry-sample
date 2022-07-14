cd512d66be3bf9feb79a9176c348392ed07000dc
K.Himeno <6715229+Himenon@users.noreply.github.com>
2022-07-14 10:45:56 +0900

    chore: add debug message

--- sentry.sh
+++ sentry.sh
@@ -1,12 +1,26 @@
 # !/bin/bash
 
-echo "APP_VERSION        = $APP_VERSION"
-echo "SENTRY_PROJECT     = $SENTRY_PROJECT"
-echo "SENTRY_ENVIRONMENT = $SENTRY_ENVIRONMENT"
+echo "Parameter"
+echo "  APP_VERSION        = $APP_VERSION"
+echo "  SENTRY_PROJECT     = $SENTRY_PROJECT"
+echo "  SENTRY_ENVIRONMENT = $SENTRY_ENVIRONMENT"
 
+echo ""
+
+echo "> sentry-cli releases deploys"
 npx sentry-cli releases deploys "$APP_VERSION" new -e $SENTRY_ENVIRONMENT
+
+echo "> sentry-cli releases new"
 npx sentry-cli releases new -p $SENTRY_PROJECT $APP_VERSION
+
+echo "> sentry-cli releases new files"
 npx sentry-cli releases -p $SENTRY_PROJECT files $APP_VERSION upload-sourcemaps ./dist/assets --rewrite --url-prefix '~/vite-sentry-sample/assets'
-npx sentry-cli releases set-commits --auto $APP_VERSION
+
+echo "> sentry-cli releases set-commits --auto"
+npx sentry-cli releases set-commits --auto $APP_VERSION --ignore-missing 
+
+echo "> sentry-cli releases finalize"
 npx sentry-cli releases finalize $APP_VERSION
+
+echo "> sentry-cli releases deploys"
 npx sentry-cli releases deploys $APP_VERSION new -e $SENTRY_ENVIRONMENT
